aba3aef8da21ddabbcf89c981e7ae15bdf397380
Alex Wiltschko <Alex@dhc027590.med.harvard.edu>
2012-04-02 18:45:57 -0400

    Initial Commit

---
+++ Novocaine/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Novocaine
+//
+//  Created by Alex Wiltschko on 4/2/12.
+//  Copyright (c) 2012 Datta Lab, Harvard University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Novocaine/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  Novocaine
+//
+//  Created by Alex Wiltschko on 4/2/12.
+//  Copyright (c) 2012 Datta Lab, Harvard University. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return YES;
+    }
+}
+
+@end
