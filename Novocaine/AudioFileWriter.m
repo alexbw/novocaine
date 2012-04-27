b135bea64e8c6e8474a22ec2e2d3c003b2ed77f8
Alex Wiltschko <Alex@Alexs-MacBook-Air.local>
2012-04-27 13:25:52 -0400

    Sketches of an AudioFileWriter

---
+++ Novocaine/AudioFileWriter.m
@@ -0,0 +1,13 @@
+//
+//  AudioFileWriter.m
+//  Novocaine
+//
+//  Created by Alex Wiltschko on 4/27/12.
+//  Copyright (c) 2012 Datta Lab, Harvard University. All rights reserved.
+//
+
+#import "AudioFileWriter.h"
+
+@implementation AudioFileWriter
+
+@end
