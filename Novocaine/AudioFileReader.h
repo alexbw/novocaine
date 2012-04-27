b694b3951d1c54d9efe9a7790b8c17663a0aad0f
Alex Wiltschko <Alex@Alexs-MacBook-Air.local>
2012-04-27 08:40:44 -0400

    Working on an AudioFileReader class

---
+++ Novocaine/AudioFileReader.h
@@ -0,0 +1,13 @@
+//
+//  AudioFileReader.h
+//  Novocaine
+//
+//  Created by Alex Wiltschko on 4/27/12.
+//  Copyright (c) 2012 Datta Lab, Harvard University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AudioFileReader : NSObject
+
+@end
---
+++ Novocaine/AudioFileReader.m
@@ -0,0 +1,13 @@
+//
+//  AudioFileReader.m
+//  Novocaine
+//
+//  Created by Alex Wiltschko on 4/27/12.
+//  Copyright (c) 2012 Datta Lab, Harvard University. All rights reserved.
+//
+
+#import "AudioFileReader.h"
+
+@implementation AudioFileReader
+
+@end
