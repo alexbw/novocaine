716cba0e078e5bc635d80b18b88679ffa615ffe2
Nick Donaldson <ndonald2@gmail.com>
2013-07-18 16:52:58 -0400

    Device playack fix for interleaved channel mappings

--- Novocaine/Novocaine.m
+++ Novocaine/Novocaine.m
@@ -792,7 +792,14 @@ OSStatus renderCallback (void						*inRefCon,
                 int thisNumChannels = ioData->mBuffers[iBuffer].mNumberChannels;
                 
                 for (int iChannel = 0; iChannel < thisNumChannels; ++iChannel) {
-                    vDSP_vsadd(sm.outData+iChannel+iBuffer, sm.numOutputChannels, &zero, (float *)ioData->mBuffers[iBuffer].mData, thisNumChannels, inNumberFrames);
+
+                    int interleaveOffset = iChannel;
+                    if (iBuffer < sm.numOutputChannels){
+                        interleaveOffset += iBuffer;
+                    }
+                    
+                    vDSP_vsadd(sm.outData+interleaveOffset, sm.numOutputChannels, &zero, (float *)ioData->mBuffers[iBuffer].mData, thisNumChannels, inNumberFrames);
+                    
                 }
             }
         }
